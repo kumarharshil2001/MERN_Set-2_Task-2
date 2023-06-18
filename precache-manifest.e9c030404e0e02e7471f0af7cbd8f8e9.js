f580036e91d24471f90ac7429b19d4429c87aedf
kumarharshil2001 <kumarharshil2001@gmail.com>
2023-06-18 22:27:26 +0530

    Updates

---
+++ precache-manifest.e9c030404e0e02e7471f0af7cbd8f8e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00e13d368c24432d925cba77b39c62c7",
+    "url": "/MERN_Set-2_Task-2/index.html"
+  },
+  {
+    "revision": "b3c30e879b6a1f88f1a7",
+    "url": "/MERN_Set-2_Task-2/static/css/main.2f3f4aff.chunk.css"
+  },
+  {
+    "revision": "3c8d2e142aa2ea988c0e",
+    "url": "/MERN_Set-2_Task-2/static/js/2.772c2dd3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MERN_Set-2_Task-2/static/js/2.772c2dd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3c30e879b6a1f88f1a7",
+    "url": "/MERN_Set-2_Task-2/static/js/main.b24c4d33.chunk.js"
+  },
+  {
+    "revision": "24c02b0ee47f7accdf63",
+    "url": "/MERN_Set-2_Task-2/static/js/runtime-main.5868ef42.js"
+  }
+]);
